1b03ecee0d6d2e13f29b6282a8dfa2a7768cbf8f
Stas Rudakou <stas@garage22.net>
2017-07-05 09:56:00 +0300

    Start reading from dht22 (without saving the result yet)

--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,87 @@
+extern crate dht22_pi;
+
 use std::sync::{Arc, RwLock};
-use std::thread::JoinHandle;
+use std::time;
+use std::thread;
 
 #[derive(Debug, Clone)]
 pub struct DataValue {
     pub temperature: f32,
-    pub humidity: f32
+    pub humidity: f32,
 }
 
 #[derive(Debug, Clone)]
 pub enum DataErrorKind {
     Timeout,
     Checksum,
-    IO
+    IO,
 }
 
 #[derive(Debug, Clone)]
 pub struct DataError {
     pub last_error: DataErrorKind,
-    pub error_count: usize
+    pub error_count: usize,
 }
 
 #[derive(Debug, Clone)]
 pub struct Observation {
     pub data_point: Option<DataValue>,
-    pub data_error: Option<DataError>
+    pub data_error: Option<DataError>,
 }
 
 impl Observation {
     fn new() -> Self {
-        Observation { data_point: None, data_error: None }
+        Observation {
+            data_point: None,
+            data_error: None,
+        }
     }
 }
 
-pub struct Sensor {
+trait Sensor {
+    fn get_observation(self) -> Result<Observation, &'static str>;
+}
+
+pub struct AsyncDhtSensor {
     observation: Arc<RwLock<Observation>>,
     pin: u8,
-    handle: Option<JoinHandle<()>>
+    handle: Option<thread::JoinHandle<()>>,
 }
 
-impl Sensor {
+impl AsyncDhtSensor {
     fn new(pin: u8) -> Self {
         let observation = Arc::new(RwLock::new(Observation::new()));
-        Sensor { observation: observation, pin: pin, handle: None }
-    }
+        let handle = thread::spawn(move || {
+            thread::sleep(time::Duration::from_secs(10));
+            dht22_pi::read(pin).unwrap();
+        });
 
-    pub fn get_observation(self) -> Result<Observation, & 'static str> {
+        AsyncDhtSensor {
+            observation: observation,
+            pin: pin,
+            handle: Some(handle),
+        }
+    }
+}
+
+impl Sensor for AsyncDhtSensor {
+    fn get_observation(self) -> Result<Observation, &'static str> {
         match self.observation.read() {
             Ok(value) => Ok(value.clone()),
-            Err(_) => Err("Sensor is poisoned")
+            Err(_) => Err("Sensor is poisoned"),
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use ::Sensor;
+    use AsyncDhtSensor;
+    use Sensor;
 
     #[test]
     fn new_sensor_is_readable() {
         let pin = 0;
-        let sensor = Sensor::new(pin);
+        let sensor = AsyncDhtSensor::new(pin);
         let observation = sensor.get_observation();
         assert!(observation.is_ok());
     }
