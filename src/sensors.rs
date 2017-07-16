32b52a5da8562b41eff714a0072342288a9624b6
Stas Rudakou <stas@garage22.net>
2017-07-16 16:58:30 +0300

    Add SequenceSensor that can be used for testing

--- src/sensors.rs
+++ src/sensors.rs
@@ -1,4 +1,6 @@
 use std::panic;
+use std::cell::Cell;
+use std::vec::Vec;
 
 use dht22_pi;
 
@@ -109,9 +111,43 @@ impl Sensor for ErrSensor {
     }
 }
 
+/// Sensor that always returns a predefined sequence of values.
+///
+/// Only used for testing.
+pub struct SequenceSensor {
+    sequence: Vec<Result<DataPoint, ErrorKind>>,
+    position: Cell<usize>,
+}
+
+impl SequenceSensor {
+    /// Create a new testing sensor instance.
+    pub fn new(sequence: Vec<Result<DataPoint, ErrorKind>>) -> Self {
+        SequenceSensor {
+            sequence: sequence,
+            position: Cell::new(0),
+        }
+    }
+}
+
+impl Sensor for SequenceSensor {
+    fn read(&self) -> Result<DataPoint, ErrorKind> {
+        let index = self.position.get();
+        match self.sequence.get(index) {
+            Some(value) => {
+                self.position.set(index + 1);
+                value.clone()
+            }
+            None => Result::Err(ErrorKind::Runtime),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use sensors::{ErrorKind, ErrSensor, GpioSensor, OkSensor, Sensor};
+    use sensors::{DataPoint, ErrorKind, ErrSensor, GpioSensor, OkSensor, Sensor, SequenceSensor};
+
+    const TEST_TEMPERATURE: f64 = 24.0;
+    const TEST_HUMIDITY: f64 = 55.1;
 
     #[test]
     fn gpiosensor_smoke() {
@@ -122,9 +158,7 @@ mod tests {
 
     #[test]
     fn oksensor_smoke() {
-        let temperature = 24.0;
-        let humidity = 55.1;
-        let sensor = OkSensor::new(temperature, humidity);
+        let sensor = OkSensor::new(TEST_TEMPERATURE, TEST_HUMIDITY);
         assert!(sensor.read().is_ok());
     }
 
@@ -133,5 +167,21 @@ mod tests {
         let error = ErrorKind::Integrity;
         let sensor = ErrSensor::new(error.clone());
         assert_eq!(sensor.read(), Result::Err(error));
+    }
+
+    #[test]
+    fn sequencesensor_smoke() {
+        let mut results = Vec::new();
+        let value0 = Result::Err(ErrorKind::IO);
+        results.push(value0.clone());
+        let value1 = Result::Ok(DataPoint::new(TEST_TEMPERATURE, TEST_HUMIDITY));
+        results.push(value1.clone());
+
+        let sensor = SequenceSensor::new(results);
+
+        assert_eq!(sensor.read(), value0);
+        assert_eq!(sensor.read(), value1);
+        assert_eq!(sensor.read(), Result::Err(ErrorKind::Runtime));
+        assert_eq!(sensor.read(), Result::Err(ErrorKind::Runtime));
     }
 }
